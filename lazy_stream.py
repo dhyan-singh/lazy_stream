e0f23bf6ca1f6a5a01071c8ef0d090dfc234e18d
Dhyan Singh <dhyansingh24042001@gmail.com>
2025-03-13 21:36:24 +0530

    basic python lazy_stream implementation

---
+++ lazy_stream.py
@@ -0,0 +1,79 @@
+from dataclasses import dataclass
+from typing import Callable, Generic
+from abc import ABC, abstractmethod
+from itertools import islice
+
+class Stream[T](ABC):
+    @abstractmethod
+    def head(self) -> T:
+        pass
+
+    @abstractmethod
+    def tail(self) -> "Stream[T]":
+        pass
+
+    @abstractmethod
+    def is_empty(self) -> bool:
+        pass
+
+    def of(*l):
+        if l:
+            return Stream.cons(lambda: l[0], lambda: Stream.of(*l[1:]))
+        else:
+            return Empty[T]()
+
+    def cons(hd: Callable[[], T], tl: Callable[[], "Stream[T]"]):
+        return Cons(hd, tl)
+
+    def toList(self) -> list[T]:
+        # let's just consume here
+        l = []
+        n = self
+        while True:
+            match type(n).__name__:
+                case "Cons":
+                    l.append(n.head()())
+                case "Empty":
+                    return l
+                case _:
+                    raise Exception("There are supposed to be only two types? How did I reach here??")
+            n = n.tail()()
+            
+
+        # print(self.head()())
+        # print(self.tail()().head()())
+        # print(self.tail()().tail()().tail().head()())
+
+
+class Empty[T](Stream[T]):
+    def head(self) -> T:
+        raise Exception("Empty Stream")
+    
+    def tail(self) -> "Stream[T]":
+        raise Exception("Empty Stream")
+    
+    def is_empty(self) -> bool:
+        return True
+
+@dataclass
+class Cons[T](Stream[T]):
+    def __init__(self, head: Callable[[], T], tail: Callable[[], Stream[T]]):
+        self._head = head
+        self._tail = tail
+
+    def head(self) -> T:
+        return self._head
+    
+    def tail(self) -> "Stream[T]":
+        return self._tail
+    
+    def is_empty(self) -> bool:
+        return False
+
+def lazy():
+    yield 5
+
+if __name__ == "__main__":
+    Cons[int](lambda: 5, lambda: Empty[int]())
+    print(Stream[int].of(5, 7, 3, 4).toList())
+    # Stream[int].of()
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Lazy Stream
+
+Implementing lazy evlaution in Python.
+
+Can I port Functional Programming in Kotlin's Chapter 5 Lazy Stream in Python.
+
+let's see.
+
+### Python's lazy evaluation
+
+Python evaluates things lazily by using `yield` keyword. I don't know what the fuck these Kotlin people came up with.
+
+So implementing lazy in python is very easy, but how to I implement these specific `Stream<out A>` class to python.
